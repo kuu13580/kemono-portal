f7f44f4595564daa06ab084cb6d516553ffc3e7a
kuu13580 <kuu13580@gmail.com>
2023-04-09 20:51:26 +0900

    TopPageを仮実装

---
+++ fe/src/pages/TopPage.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import NavBar from "../components/NavBar";
+
+const TopPage = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Typography>
+        未実装
+      </Typography>
+    </>
+  );
+}
+
+export default TopPage;
