942552a4dc45fbc3df98a54baec05e313899c8cb
kuu13580 <kuu13580@gmail.com>
2023-04-09 21:25:12 +0900

    Routerの表記変更

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Top from './pages/Top';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Top />} />
           <Route path="/profile" element={<><NavBar/>未実装プロフィール</>} />
           <Route path="/account" element={<><NavBar/>未実装アカウント</>} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
